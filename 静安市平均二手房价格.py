a5ff459afa7eb5b97301dcff9f08dba9d9e5bc10
xsthunder <xsthunder@outlook.com>
2019-06-22 15:14:37 +0800

    Add files via upload

---
+++ 静安市平均二手房价格.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("https://sh.lianjia.com/ershoufang/jingan/")
+
+
+ret = []
+while(nxt!=None):
+    price_eles = driver.find_elements_by_class_name('totalPrice')
+    for ele in price_eles:
+        ret.append(ele.text)
+    try:
+        nxt = driver.find_element_by_link_text('下一页')
+        nxt.click()
+    except:
+        nxt = None
+        # NoSuchElementException                    
+        
+
+driver.close()
+
+
+nxt_ret = []
+for x in ret:
+    nxt_ret.append(float(x[:-1]))
+
+
+sum(nxt_ret)/len(nxt_ret)
+
